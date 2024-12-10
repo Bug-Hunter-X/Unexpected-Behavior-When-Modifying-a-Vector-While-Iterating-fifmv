7da28d7b5951e08c05eeacbdd29a83ed67ac12f9
BugHunter-X <askzenai@gmail.com>
2024-12-10 12:58:06 +0100

    Add bug.cpp file

---
+++ bug.cpp
@@ -0,0 +1,1 @@
+std::vector<int> vec; for (int i = 0; i < 10; ++i) { vec.push_back(i); } for (int i = 0; i < vec.size(); ++i) { vec[i] = i * 2; } for (int i = 0; i < vec.size(); ++i) { std::cout << vec[i] << " "; } std::cout << std::endl;
